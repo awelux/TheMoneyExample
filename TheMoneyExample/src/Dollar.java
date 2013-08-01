6f45ac9c0030483fb4d357a550ee11d51b3451bc
Awelux <geronimo@inetmx.de>
2013-08-01 18:54:14 +0200

    Chapter 3
    Succeed with equality test
    
    - Add fake implementation

--- TheMoneyExample/src/Dollar.java
+++ TheMoneyExample/src/Dollar.java
@@ -3,12 +3,17 @@ public class Dollar {
 
 	public int amount;
 
-	public Dollar(int amount) {
+	public Dollar(final int amount) {
 		this.amount = amount;
 	}
 
-	public Dollar times(int multiplier) {
+	public Dollar times(final int multiplier) {
 		return new Dollar(amount * multiplier);
+	}
+
+	@Override
+	public boolean equals(final Object object) {
+		return true;
 	}
 
 }
