ad5af9fdd96cca1c200d49a11edbeee025524046
Awelux <geronimo@inetmx.de>
2013-08-01 18:37:33 +0200

    Chapter 3
    Add equality test

--- TheMoneyExample/test/DollarTest.java
+++ TheMoneyExample/test/DollarTest.java
@@ -1,4 +1,5 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,14 +9,18 @@ public class DollarTest {
 	@Test
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		
+
 		Dollar product = five.times(2);
-		
+
 		assertEquals(10, product.amount);
-		
+
 		product = five.times(3);
-		
+
 		assertEquals(15, product.amount);
 	}
 
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+	}
 }
